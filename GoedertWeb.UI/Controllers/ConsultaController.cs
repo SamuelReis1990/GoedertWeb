ce1edb8d971c759ed9bf5d9b09bab408de8b5e3b
Samuel_reis <samuelreis.1990@gmail.com>
2017-12-24 21:32:08 -0200

    Commit inicial

---
+++ GoedertWeb.UI/Controllers/ConsultaController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace GoedertWeb.UI.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        // GET: Consulta
+        public ActionResult Index()
+        {
+            ViewBag.WEB_API = WebConfigurationManager.AppSettings["WEB_API"];
+            return View();
+        }
+    }
+}
---
+++ GoedertWeb.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoedertWeb.UI.MvcApplication" Language="C#" %>
