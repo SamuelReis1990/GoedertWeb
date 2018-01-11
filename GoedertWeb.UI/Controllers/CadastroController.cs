86a54bd81b6a37afaf1094a8a0e68d22db4ab48d
Samuel_reis <samuelreis.1990@gmail.com>
2018-01-11 19:05:11 -0200

    Commit com a implementação de inclusão na tela de cadastro, melhorias em outras telas

--- GoedertWeb.UI/Controllers/CadastroController.cs
+++ GoedertWeb.UI/Controllers/CadastroController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Configuration;
+﻿using GoedertWeb.UI.Models;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace GoedertWeb.UI.Controllers
@@ -10,6 +11,29 @@ namespace GoedertWeb.UI.Controllers
         {
             ViewBag.WEB_API = WebConfigurationManager.AppSettings["WEB_API"];
             return View();
+        }
+
+        public ActionResult TelaCrud(string acao)
+        {
+            ViewBag.WEB_API = WebConfigurationManager.AppSettings["WEB_API"];
+            ViewBag.IND_ACAO = acao;
+
+            DadosPessoas model = new DadosPessoas();
+
+            model.id_tipo_pessoa = "";
+            model.sexo = "";
+
+            return PartialView("_partialCrud", model);
+        }
+
+        public ActionResult WebCam(string acao)
+        {
+            ViewBag.WEB_API = WebConfigurationManager.AppSettings["WEB_API"];
+            ViewBag.ACAO = acao;
+
+            DadosPessoas model = new DadosPessoas();
+
+            return PartialView("_webCam", model);
         }
     }
 }
